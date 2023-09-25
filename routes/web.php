f0c9fc134cdbc230efa4499132e5e2293529b8a6
RubinKK <suelaineb04@gmail.com>
2023-09-25 19:11:13 +0900

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\Listing;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,73 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/hello', function () {
+//     return response('<h1>Hello World</h1>', 200)
+//         ->header('Content-Type', 'text/plain');
+// });
+
+// Route::get('/posts/{id}', function($id) {
+//     return response('Post ' . $id); // use . to concatenate stings
+// })->where('id','[0-9]+'); // only one or more digits (numeric characters) will be accepted, other chars will return not found
+
+// Route::get('/search', function(Request $request) {
+//     return $request->name . ' ' . $request->city;
+// });
+
+
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+// Single Listing
+// Route::get('/listings/{id}', function($id) {
+//     if($listing) {
+//         return view('listing', [
+//             'listing' => $listing
+//         ]);
+//     } else {
+//         abort('404');
+//     }
+// });
+
+// Route::get('/listings/{id}', function($id) {
+//     return view('listing',[
+//         'listing'=> Listing::find($id)
+//     ]);
+// })->where('id','[0-9]+');
+
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+// Manage Listings
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
+// Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+// Don't know why but this one has to be below otherwise will cause error
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
